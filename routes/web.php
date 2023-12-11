7f933b372899e402aba528d32173203e2f73af04
masato <04masato05@gmail.com>
2023-12-11 19:30:39 +0900

    イベントの作成&View側の作成

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use \App\Events\MyEvent;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome', ['key' => env('PUSHER_APP_KEY'), 'cluster' => env('PUSHER_APP_CLUSTER')]);
+});
+
+Route::get('/test', function () {
+    event(new MyEvent('Hello World'));
 });
